cfef0f7427008a8ebb9481223d5179b5df62a35e
Nick <sshedl22@gmail.com>
2019-01-31 15:30:46 +0300

    New lesson

--- CoffeeMachine/src/Main.java
+++ CoffeeMachine/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int v1 = scanner.nextInt();
+        int v2 = scanner.nextInt();
+        int v3 = scanner.nextInt();
+        int v4 = (v1 + v2 + v3 + 1) / 2;
+        System.out.println(v4);
     }
 }
