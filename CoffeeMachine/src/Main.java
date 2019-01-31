3568245683c5abde83cc84276bbf5ee80c653dc0
Nick <sshedl22@gmail.com>
2019-01-31 16:12:01 +0300

    1st version

--- CoffeeMachine/src/Main.java
+++ CoffeeMachine/src/Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
-
+//One cup of coffee made on this coffee machine contains 200 ml of water, 50 ml of milk, and 15 g of coffee beans.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int v1 = scanner.nextInt();
-        int v2 = scanner.nextInt();
-        int v3 = scanner.nextInt();
-        int v4 = (v1 + v2 + v3 + 1) / 2;
-        System.out.println(v4);
+        System.out.println("Write how many cups of coffee you will need: ");
+        int numberOfCups = scanner.nextInt();
+        int amountOfWater = numberOfCups * 200;
+        int amountOfMilk = numberOfCups * 50;
+        int amountOfBeans = numberOfCups * 15;
+        System.out.println("For " + numberOfCups + " cups of coffee you will need:");
+        System.out.println(amountOfWater + " ml of water");
+        System.out.println(amountOfMilk + " ml of milk");
+        System.out.println(amountOfBeans + " g of coffee beans");
     }
 }
