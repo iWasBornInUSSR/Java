665a4765cacd622488a99340f447d3264601b1ba
Nick <sshedl22@gmail.com>
2019-03-09 21:09:56 +0300

    Liner eq v2

--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -7,7 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
-        double ans = b/a;
-        System.out.println(ans);
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+        double f = scanner.nextDouble();
+        double y = (f - c * d / a) / (e - b * d / a);
+        double x = (c - b * y) / a;
+        System.out.printf("%4f %4f",x,y);
     }
 }
