504ced33a5debff418c999855fd78b9b516afb18
Nick <sshedl22@gmail.com>
2019-04-05 00:39:22 +0300

    Add arguments

--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -1,23 +1,15 @@
 package solver;
 
-import java.io.PrintStream;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        double a = scanner.nextDouble();
-//        double b = scanner.nextDouble();
-//        double c = scanner.nextDouble();
-//        double d = scanner.nextDouble();
-//        double e = scanner.nextDouble();
-//        double f = scanner.nextDouble();
-//        double y = (f - c * d / a) / (e - b * d / a);
-//        double x = (c - b * y) / a;
-//        System.out.printf("%4f %4f", x, y);
-        System.out.println("Start solving the equation.\n" +
-                "Rows manipulation:");
-        Matrix matrix = new Matrix("/home/picknick/IdeaProjects/HSproj/Linear Equations Solver/Linear Equations Solver/task/src/solver/in.txt");
-        matrix.solve("/home/picknick/IdeaProjects/HSproj/Linear Equations Solver/Linear Equations Solver/task/src/solver/out.txt");
+        if (args.length < 4)
+            return;
+        if (args[0].equals("-in")) {
+            Matrix matrix = new Matrix(args[1]);
+            System.out.println("Start solving the equation.\n" +
+                    "Rows manipulation:");
+            if(args[2].equals("-out"))
+            matrix.solve(args[3]);
+        }
     }
 }
